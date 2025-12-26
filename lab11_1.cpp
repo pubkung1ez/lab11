df8609221a59b347eee2a34dceb8e2e0b8750df7
pubkung1ez <tirapan_30506@sysp.ac.th>
2025-12-26 16:38:44 +0700

    suss

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,17 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <ostream>
+#include <string>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "Press Enter 3 times to reveal your future.";
+    string grade[9] = {"A", "B+", "B", "C+", "C", "D+", "D", "F","W"};
+    string text;
+    for(int i = 0 ; i < 3 ; i++){
+        getline(cin , text);
+    }
+    srand(time(0));
+    cout << "You will get " << grade[rand()%8]<< " in this 261102.";   
+}
